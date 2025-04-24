49710b8e87d6ab03b85e7d6a09736a9c0a54af66
Omkar <omkarpowar8724@gmail.com>
2025-04-25 01:04:11 +0530

    code for handle GET, POST, DELETE  this API calls on PostMan

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 7777;
 
-app.get('/', (req, res) => {
-    res.send('Home page');
-})
+// app.get('/', (req, res) => {
+//     res.send('Home page');
+// })
 
-app.get('/test', (req, res) => {
-    res.send('Test page');
-})
+// app.get('/test', (req, res) => {
+//     res.send('Test page');
+// })
 
-app.get('/hellow', (req, res) => {  
-    res.send('Hello World!');
-})
+// this will only handle the GET method
+app.get("/hellow", (req, res) => {
+  res.send({ username: "John Doe", age: 30 });
+});
+
+app.get("/hellow", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/hellow", (req, res) => {
+  res.send("data posted successfully");
+});
+
+app.delete("/hellow", (req, res) => {
+  res.send("data deleted successfully");
+});
+
+//this will match all the HTTP API calls to Test
+app.use("/test", (req, res, next) => {
+  console.log("Test middleware");
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-})
+  console.log(`Server is running on http://localhost:${port}`);
+});

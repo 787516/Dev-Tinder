a46b9ed5b9d6941e910e4f568f4933080ed8111c
Omkar <omkarpowar8724@gmail.com>
2025-05-03 00:02:09 +0530

    Diving into API

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ const port = 7777;
 const connectDB = require("./config/Database");
 const User = require("./models/user");
 
+// Middleware to parse URL-encoded data
+app.use(express.json());
+
+//POST-API Middleware to parse JSON data
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Omkar",
-    lastName: "Powar",
-    emailId: "Omkar123@gmail.com",
-    password: "1234567",
-    age : 25,
-  });
+  const user = new User(req.body);
+  console.log("data", user);
 
   try {
     await user.save();
@@ -20,6 +19,105 @@ app.post("/signup", async (req, res) => {
     res.status(400).send("Error creating user: " + error.message);
   }
 });
+
+//GET-API Middleware to get user by emailId
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found!");
+    } else {
+      res.status(200).send(user);
+      console.log("User found:", user);
+    }
+  } catch (error) {
+    res.status(500).send("Error fetching user: " + error.message);
+  }
+});
+
+//GET-API Middleware to get all users
+app.get("/feed", async (req, res) => {
+  const AllUsers = await User.find({});
+  try {
+    if (!AllUsers) {
+      res.status(404).send("No users found!");
+    } else {
+      res.status(200).send(AllUsers);
+      console.log("All users found:", AllUsers);
+    }
+  } catch (error) {
+    res.status(500).send("Error fetching users: " + error.message);
+  }
+});
+
+//GET-API Middleware to get user by id
+app.get("/userID", async (req, res) => {
+  const userID = req.body._id;
+  try {
+    const user = await User.findById(userID);
+    if (!user) {
+      res.status(404).send("User not found!");
+    } else {
+      res.status(200).send(user);
+      console.log("User found:", user);
+    }
+  } catch (error) {
+    res.status(500).send("Error fetching user: " + error.message);
+  }
+});
+
+//delete-API Middleware to delete user by emailId
+app.delete("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.findOneAndDelete({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found!");
+    } else {
+      res.status(200).send("User deleted successfully!");
+      console.log("User deleted:", user);
+    }
+  } catch (error) {
+    res.status(500).send("Error deleting user: " + error.message);
+  }
+})
+
+
+ app.patch("/user", async (req, res) => {
+  const UserID = req.body.UserID;
+  const userData = req.body;
+  try {
+    const user = await User.findOneAndUpdate({ _id: UserID}, userData);
+    console.log("User updated:", user);
+    if (!user) {
+      res.status(404).send("User not found!");
+    } else {
+      res.status(200).send("User updated successfully!");
+    }
+  }
+  catch (error) {
+    res.status(500).send("Error updating user: " + error.message);
+  }
+});
+
+// app.patch("/user", async (req, res) => {
+//   const { userId, updateData } = req.body;
+//   try {
+//     const user = await User.findOneAndUpdate(
+//       { _id: userId },updateData, );
+//     console.log("User updated:", user);
+//     if (!user) {
+//       res.status(404).send("User not found!");
+//     } else {
+//       res.status(200).send("User updated successfully!");
+//     }
+//   } catch (error) {
+//     res.status(500).send("Error updating user: " + error.message);
+//   }
+// });
+
+   
 
 connectDB()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,28 +1,78 @@
 const mangoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mangoose.Schema(
   {
     firstName: {
       type: String,
       required: true,
+      minlength: 2,
+      maxlength: 20,
+      trim: true,
     },
     lastName: {
       type: String,
       required: true,
+      minlength: 3,
+      maxlength: 20,
+      trim: true,
     },
     emailId: {
       type: String,
       required: true,
-      unique: true,
+      unique: false,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address");
+        }
+      },
     },
     password: {
       type: String,
       required: true,
+      minlength: 4,
+      maxlength: 20,
+      trim: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error(
+            "Password is not strong enough. It should contain at least 8 characters, including uppercase, lowercase, numbers, and symbols."
+          );
+        }
+      },
     },
+    gender: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate: {
+        validator: function (value) {
+          return ['male', 'female', 'other'].includes(value);
+        },
+        message: props => `${props.value} is not a valid gender value`
+      }
+    },
+    
     age: {
       type: Number,
       required: true,
+      min: 18,
+      max: 80,
     },
+    photoURL: {
+      type: String,
+      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-978409_960_720.png",
+      validate: {
+        validator: function (value) {
+          return validator.isURL(value);
+        },
+        message: props => `${props.value} is not a valid URL`
+      }
+    },
+    
   },
   {
     collection: "Dev-Tinder Data", // ✅ now it’s correctly placed
